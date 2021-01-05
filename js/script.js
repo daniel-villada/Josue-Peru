f9af65494a83aa7dbce5e1122593546c426f72da
daniel-villada <danielvillada980@gmail.com>
2021-01-05 14:06:17 -0500

    Ojala se pueda el boton

--- js/script.js
+++ js/script.js
@@ -12,15 +12,3 @@ var downloadTimer = setInterval(function () {
       "<span style='color:#000; font-size: 15pt'> &#9196; AHORA PUEDES HACER CLIC &#9196;</span>";
   }
 }, 1000);
-
-function muestra(id) {
-  if (document.getElementById) {
-    //se obtiene el id
-    var contenido = document.getElementById(id); //se define la variable "el" igual a nuestro div
-    contenido.style.display === "none" ? "block" : "none"; //damos un atributo display:none que oculta el div
-  }
-}
-window.onload = function () {
-  /*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente*/
-  muestra("contenido");
-};
